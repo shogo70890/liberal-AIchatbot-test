d67f4c5e5a9af7265953c53dae1bbe371d5b7fad
shogo70890 <itosyo10@outlook.jp>
2025-08-22 15:11:25 +0900

    update app

--- constants.py
+++ constants.py
@@ -58,9 +58,6 @@ SUPPORTED_EXTENSIONS = {
     ".csv": lambda path: CSVLoader(path, encoding="utf-8"),
     ".txt": lambda path: TextLoader(path, encoding="utf-8")
 }
-WEB_URL_LOAD_TARGETS = [
-    "https://generative-ai.web-camp.io/"
-]
 
 
 # ==========================================
